f9148ca87fdb85a3361b88defcec5f3ae58a02f6
Bridger Thompson <bridgerat00@gmail.com>
2023-12-06 15:03:10 -0700

    navbar and logout

--- client/src/components/navBar/NavBar.tsx
+++ client/src/components/navBar/NavBar.tsx
@@ -4,8 +4,10 @@ import { ThemeSelector } from "./ThemeSelector";
 import { useContext } from "react";
 import { ChildContext } from "../../context/childContext";
 import { useGetChildQuery, useGetChildsPointsQuery } from "../../hooks/peopleHooks";
+import { useAuth } from "react-oidc-context";
 
 export const NavBar = () => {
+  const auth = useAuth();
   const { selectedChildId } = useContext(ChildContext);
   const childQuery = useGetChildQuery(selectedChildId);
   const pointsQuery = useGetChildsPointsQuery(selectedChildId);
@@ -22,11 +24,29 @@ export const NavBar = () => {
     }
   }
 
+  const logout = () => {
+    auth.removeUser().then(() => auth.signoutRedirect());
+  };
+
   return (
     <nav className="bg-primary shadow">
       <div className="row mx-0">
+        <div className="row justify-content-center d-lg-none">
+          <div className="btn-group">
+            {routes.map((l) => (
+              <button
+                key={l.to}
+                className={`btn btn-primary ${location.pathname === l.to && "active"}`}
+                onClick={() => navigate(l.to)}
+              >
+                <i className={`${l.icon} fs-4`} />
+                <div className="small">{l.text}</div>
+              </button>
+            ))}
+          </div>
+        </div>
         {routes.map((r) => (
-          <div className="col-auto pe-0 my-auto"
+          <div className="col-auto pe-0 my-auto d-none d-lg-block"
             key={r.to}>
             <button className={`btn btn-primary ${isActive(r.to) && "active"}`}
               onClick={() => navigate(r.to)}>
@@ -34,7 +54,7 @@ export const NavBar = () => {
             </button>
           </div>
         ))}
-        <div className="text-end col my-auto">
+        <div className="text-lg-end col my-auto">
           {(window.location.pathname.includes("prizes") || window.location.pathname.includes("chores")) && (
             <div className="fw-bold fs-4">
               <span className={`rounded px-2 fst-italic text-${childQuery.data?.cardColor ?? "success"} bg-primary-subtle`}>
@@ -45,6 +65,10 @@ export const NavBar = () => {
         </div>
         <div className="col-auto">
           <ThemeSelector />
+        </div>
+        <div className="col-auto my-auto">
+          <button className="btn btn-danger"
+            onClick={logout}>Logout</button>
         </div>
       </div>
     </nav>
